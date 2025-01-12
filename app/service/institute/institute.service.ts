e610ab115fea2c812c89c64fbac728a287a6a8af
Saurav Poojary <sauravpoojary@gmail.com>
2025-01-12 16:34:51 +0530

    fix: institute creation

--- app/service/institute/institute.service.ts
+++ app/service/institute/institute.service.ts
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { TEAM_TYPE } from "../../common/enum";
 import { InstituteRequest } from "../../models/institute/institute.interface";
+import { TeamRequest } from "../../models/team/team.interface";
 import supabaseInstituteSdk from "../../sdk/institute/supabase.institute.sdk";
+import supabaseTeamSdk from "../../sdk/team/supabase.team.sdk";
 
 const createOrUpdate = async (instituteRequest: InstituteRequest) => {
   const existingInstitute = await supabaseInstituteSdk.getInstituteByName(
@@ -14,6 +17,19 @@ const createOrUpdate = async (instituteRequest: InstituteRequest) => {
     );
   } else {
     response = await supabaseInstituteSdk.createInstitute(instituteRequest);
+    if (response && response.institute_id) {
+      const teamRequests: TeamRequest[] = [
+        {
+          institute_id: response.institute_id,
+          team_type: TEAM_TYPE.HIGH_PERFORMANCE,
+        },
+        {
+          institute_id: response.institute_id,
+          team_type: TEAM_TYPE.DEVELOPMENT,
+        },
+      ];
+      await supabaseTeamSdk.createTeam(teamRequests);
+    }
   }
   return response;
 };
