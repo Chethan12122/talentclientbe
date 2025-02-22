fa0b38b74f30d7b2664e71ffcc9d16f6c8e47080
hrithik4201 <hrithikkumar2000@gmail.com>
2025-02-22 19:51:22 +0530

    added log

--- app/common/google-auth.ts
+++ app/common/google-auth.ts
@@ -1,8 +1,10 @@
 import { google } from "googleapis";
 import { config } from "./config";
+import { logger } from "./logger";
 
 // Load credentials from the service account key JSON
 const credentials = config.googleAuth.json || {};
+logger.info("credentials", credentials);
 
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 
